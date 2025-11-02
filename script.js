2ce1f184c54a024ca4d07154e80057c080384152
shelbynas <gabrielsapo119@gmail.com>
2025-11-02 01:04:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // ===================================================
 
 // ⚠️ ATENÇÃO: CHAVE DA API ATUALIZADA AQUI
-const API_KEY = "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"; 
+const API_KEY = "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"; 
 
 const GROQ_ENDPOINT = "https://api.groq.com/openai/v1/chat/completions";
 const MODEL_NAME = "llama-3.1-8b-instant"; 
